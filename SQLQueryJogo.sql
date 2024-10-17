91adcea276e5d1b025ef6f02ab5ec6ee5b7c4e21
Matheus Santos <142439903+Matheus12Santos@users.noreply.github.com>
2024-10-17 12:32:29 -0300

    Feat: Textos da historia atualizados.

---
+++ SQLQueryJogo.sql
@@ -0,0 +1,34 @@
+CREATE DATABASE GreciaGames;
+USE GreciaGames;
+use master;
+drop database GreciaGames
+CREATE TABLE Personagens(
+    PersonId INT NOT NULL PRIMARY KEY IDENTITY,
+	PersonName VARCHAR(30) NOT NULL,
+    PersonTexto VARCHAR(200) NOT NULL,
+	PersonOpc1 VARCHAR(100) NOT NULL,
+	PersonOpc2 VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE Medidores(
+    MedidorId INT NOT NULL PRIMARY KEY IDENTITY,
+	PersonId INT NOT NULL,
+	EstatisMantimentos INT,
+	EstatisExercito INT ,
+	EstatisConfianca INT,
+	FOREIGN KEY(PersonId) REFERENCES Personagens(PersonId)
+);
+
+INSERT INTO Personagens VALUES('Eryx', 'General, uma tempestade se aproxima, deveriamos esperar ela passar?', 'Abrigar-se até a tempestade passar.', 'Prosseguir para economizar tempo.');
+INSERT INTO Personagens VALUES('Calista', 'A chuva passada alagou o rio que atravessaríamos', 'Improvisar uma jangada para passar.', 'Esperar a água abaixar.');
+INSERT INTO Personagens VALUES('Agapetos', 'General, avistei pelotão inimigo proximo das nossas tropas.', 'Preparar uma emboscada', 'Contornar pelo caminho mais longo.');
+
+INSERT INTO Medidores VALUES(1, -15, 0, 25);
+INSERT INTO Medidores VALUES(1, 10, -10, -30);
+
+
+INSERT INTO Medidores VALUES(2, 10, 0, 5);
+INSERT INTO Medidores VALUES(2, 15, 0, 2);
+
+INSERT INTO Medidores VALUES(3, 30, -5, 10);
+INSERT INTO Medidores VALUES(3, -20, 0, 5);
