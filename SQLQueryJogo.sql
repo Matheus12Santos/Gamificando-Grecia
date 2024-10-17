1e00d1324d9217554d9f8fd10d656fdd403c094e
Matheus Santos <142439903+Matheus12Santos@users.noreply.github.com>
2024-10-17 12:35:42 -0300

    Feat: Atualizados os dialogos dos textos.

--- SQLQueryJogo.sql
+++ SQLQueryJogo.sql
@@ -19,9 +19,26 @@ CREATE TABLE Medidores(
 	FOREIGN KEY(PersonId) REFERENCES Personagens(PersonId)
 );
 
-INSERT INTO Personagens VALUES('Eryx', 'General, uma tempestade se aproxima, deveriamos esperar ela passar?', 'Abrigar-se até a tempestade passar.', 'Prosseguir para economizar tempo.');
-INSERT INTO Personagens VALUES('Calista', 'A chuva passada alagou o rio que atravessaríamos', 'Improvisar uma jangada para passar.', 'Esperar a água abaixar.');
-INSERT INTO Personagens VALUES('Agapetos', 'General, avistei pelotão inimigo proximo das nossas tropas.', 'Preparar uma emboscada', 'Contornar pelo caminho mais longo.');
+INSERT INTO Personagens VALUES('Agapetos', 'General, o caminho será longo, deveríamos seguir para o Oeste ou para o Noroeste.', 'Vamos, para o Oeste.', 'Continuem no Noroeste.');
+INSERT INTO Personagens VALUES('Terreno Montanhoso', 'Chegamos em um terreno montanhoso, caminhos estreitos e íngremes.', 'Contornar para um local mais seguro e mais longo.', 'Avançar cautelosamente. Não temos tempo à perder!');
+INSERT INTO Personagens VALUES('Tempestade', 'Nuvens escuras se acumulam, anunciando uma tempestade.', 'Economizaremos tempo prosseguindo na tempestade.', 'Abriguemos até a tempestade passar.');
+INSERT INTO Personagens VALUES('Eryx', 'O rio à frente está transbordando devido às chuvas recentes, o que faremos?', 'Usar madeiras para improvisar uma jangada e atravessar.', 'Esperar a água se acalmar para ultrapassar.');
+INSERT INTO Personagens VALUES('Calista', 'General, avistei pelotão inimigo próximo ao nosso acampamento.', 'Escondam-se, ficaremos de tocaia.', 'Evitaremos confronto, contornem pelo caminho mais longo.');
+INSERT INTO Personagens VALUES('Vilarejo abandonado', 'Avistamos um vilarejo, parece estar abandonado.', 'Explorem e procurem mais mantimentos.', 'Pode ser uma emboscada, continuem pelo caminho principal.');
+INSERT INTO Personagens VALUES('Perseus', 'General, chegamos numa floresta densa, com uma chance alta de conter armadilhas.', 'Separar as tropas para surpreendê-los.', 'Fiquem juntos, se eles aparecem nós os atacamos.');
+INSERT INTO Personagens VALUES('Floresta armadilheira', 'Era uma armadilha! Estamos sendo atacados, o que faremos?', 'Vamos recuar. Não há tempo de levar tudo.', 'Reforcem as defesas imediatamente.');
+INSERT INTO Personagens VALUES('Velho solitário', 'Encontramos um velho solitário pelo caminho, ele pediu um pouco de nossos mantimentos.', 'Negar a entrega de suprimentos.', 'Entregar um pouco de mantimento para ele.');
+INSERT INTO Personagens VALUES('Marios', 'Avistamos exercito inimigo perto do Vale.', 'Evitaremos ataques até o Desfiladeiro.', 'Eles não podem saber que estamos próximos, ataque-os!');
+INSERT INTO Personagens VALUES('Antonios', 'Nossos suprimentos estão quase acabando.', 'Racionaremos suprimentos e faremos caçadas perto do acampamento.', 'Buscaremos vilarejos próximos para requisitar alimentos.');
+INSERT INTO Personagens VALUES('Menelaus', 'General, estamos à muitos dias andando incansavelmente, precisamos de uma pausa.', 'Continuemos em frente, estamos quase chegando.', 'Vamos parar e descansar um pouco.');
+INSERT INTO Personagens VALUES('Velho solitário', 'Reencontramos o velho solitário, e ele quer nós retribuir.', 'Aceitar.', 'Negar.');
+INSERT INTO Personagens VALUES('Desfiladeiro', 'Após um longo percurso, chegamos ao Desfiladeiro, parece que os persas ainda não chegaram.', 'Mandar seus homens descansarem.', 'Se armar, há uma possibilidade de ser uma emboscada.');
+INSERT INTO Personagens VALUES('Mensageiro inimigo', 'Xerxes mandou mensageiros propondo a rendição do exército e entrega das armas.', 'Vem buscá-las!', 'Nós rendemos ao exercito...');
+INSERT INTO Personagens VALUES('Gregorios', 'Os persas se aproximam. O que faremos, general?', 'Atacar agora.', 'Esperar um golpe mais decisivo.');
+INSERT INTO Personagens VALUES('Alexios', 'O inimigo avança em formação.', 'Manter nossa formação defensiva.', 'Flanquear os inimigos.');
+INSERT INTO Personagens VALUES('Arqueiros inimigos', 'Arqueiros estão posicionados na colina.', 'Recuem para não sermos atingidos.', 'Procurem locais para se defenderem.');
+INSERT INTO Personagens VALUES('Muitas baixas', 'Ocasionamos diversas baixas inimigas, devemos continuar nesse ritmo.', 'Continuar com força total.', 'Ir mais na defensiva.');
+INSERT INTO Personagens VALUES('O fim da batalha', 'Estamos quase vencendo.', 'Finalizar todos os restantes.', 'Poupar os restantes.');
 
 INSERT INTO Medidores VALUES(1, -15, 0, 25);
 INSERT INTO Medidores VALUES(1, 10, -10, -30);
